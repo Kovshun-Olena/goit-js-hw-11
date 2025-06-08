4e97e938dbe72d5875da74b61627ace669754e07
Kovshun-Olena <kovshun.elena.1973@gmail.com>
2025-06-08 23:23:58 +0300

    11-hw

---
+++ src/main.js
@@ -0,0 +1,53 @@
+import { fetchImages } from './js/pixabay-api.js';
+import {
+  createGalleryMarkup,
+  hideLoader,
+  lightbox,
+} from './js/render-functions.js';
+import './css/styles.css';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('form');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const query = form.elements['search-text']?.value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a search query.',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  try {
+    const images = await fetchImages(query);
+
+    if (images.length === 0) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Sorry, there are no images matching your search query. Please, try again!',
+        position: 'topRight',
+      });
+      gallery.innerHTML = '';
+    }
+
+    gallery.innerHTML = createGalleryMarkup(images);
+
+    lightbox.refresh();
+    form.reset();
+    
+  } catch (error) {
+    console.error('Error fetching images:', error);
+  }finally {
+    hideLoader();
+  }
+});
